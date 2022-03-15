b9b6371a9250d7a31e9ba6cf1670041c83f81184
서유진 <youjin030700@gmail.com>
2022-03-15 23:12:59 +0900

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -24,9 +24,45 @@ const upload = multer({storage:storage,fileFilter:imageFilter});
 //cloudinary setup
 cloudinary.config({
     cloud_name:process.env.CLOUDNARY_CLOUD_NAME,
-    api_key:process.env.CLOUDNARY_CLOUD_API_KEY,
-    api_secret:process.env.CLOUDNARY_CLOUD_API_SECRET
+    api_key:process.env.CLOUDNARY_API_KEY,
+    api_secret:process.env.CLOUDNARY_API_SECRET
 });
 
 //middle ware
-const isLoggedIn = (req,res,next)
+const isLoggedIn = (req,res,next)=>{
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error","You need to e logged in to do that");
+    res.redirect("/user/login");
+};
+
+//Router
+router("/",isLoggedIn,(req,res)=>{
+    User.findById(req.user._id)    // 친구들의 게시글
+        .populate({
+            path:"friends",
+            populate:{
+                path:"posts",
+                model:"Post"
+            }
+        })
+        .populate("posts") //현재 사용자의 게시글
+        .exec((err,user)=>{
+            if(err){
+                console.log(err);
+                req.flash("error","there has been an error finding all posts");
+                res.render("posts/index");
+            } else {
+                let posts = [];
+                for(var i=0;i<user.friends.length;i++){
+                    for(var j=0;j<user.friends[i].posts.length;j++){
+                        posts.push(user.friends[i].posts[j]);
+                    }
+                }
+                for(var i=0;i<user.posts.length;i++){
+                    posts.push(user.posts[i]);
+                }
+            }
+        })
+})
